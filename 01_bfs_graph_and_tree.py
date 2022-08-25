bc461ebf7d3a10445a45bc09a6cc57694f40507a
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-08-25 19:50:59 +0530

    Modify 01_bfs_graph_and_tree.py

--- 01_bfs_graph_and_tree.py
+++ 01_bfs_graph_and_tree.py
@@ -7,11 +7,11 @@ class Problem(ABC):
     """
     Blueprint of the various problems i.e., Abstract class of the problems
     """
+
     def __init__(self, initial: str, goal: str = None):
-        self.initial: str = initial      # initial state
-        self.goal: str = goal            # goal state
-        
-        
+        self.initial: str = initial  # initial state
+        self.goal: str = goal  # goal state
+
     @abstractmethod
     def actions(self, state: str):
         """
@@ -21,8 +21,7 @@ class Problem(ABC):
         :raises ImplementationError: If not implemented in the extended class 
         """
         pass
-    
-    
+
     def goal_test(self, state: str) -> bool:
         """
         Test for the state is the goal state or not
@@ -33,12 +32,12 @@ class Problem(ABC):
         return state == self.goal
 
 
-
 class GraphProblem(Problem):
     """
     Extends from the abstract class Problem, it represents the Graph related problems
     like Travelling in Romania problem, etc.
     """
+
     def __init__(self, initial: str, goal: str, graph: Graph):
         """
         constructor of the GraphProblem
@@ -48,9 +47,8 @@ class GraphProblem(Problem):
         :param graph: Graph instance of the problem
         """
         super().__init__(initial, goal)
-        self.graph = graph  
-        
-        
+        self.graph = graph
+
     def actions(self, A: str) -> list:
         """
         neighbor or child of the parent
@@ -59,7 +57,6 @@ class GraphProblem(Problem):
         :returns: list of the children
         """
         return list(self.graph.get(A))
-    
 
 
 class Graph:
@@ -67,6 +64,7 @@ class Graph:
     Graph class
     
     """
+
     def __init__(self, graph_dict: dict = None, directed: bool = True):
         """
         constructor of the Graph
@@ -74,22 +72,21 @@ class Graph:
         :param graph_dict: dictionary representation of the problem
         :param directed: if the dictionary is directed or not
         """
-        self.graph_dict: dict = graph_dict or {}     
+        self.graph_dict: dict = graph_dict or {}
         self.directed: bool = directed
-        
+
         # convert the directed graph into the undirected one
         if not directed:
             self.make_undirected()
-            
-            
+
     def make_undirected(self):
         """
         Algorithm to converts the directed graph into an undirected one.
 
         """
-        for parent in list(self.graph_dict.keys()):    # parent: keys of the dict
-            for child in self.graph_dict[parent]:      # each child of the parent 
-                
+        for parent in list(self.graph_dict.keys()):  # parent: keys of the dict
+            for child in self.graph_dict[parent]:  # each child of the parent
+
                 # if the child is in the dict i.e., there exists a key in the dict named as the current child
                 if child in self.graph_dict:
                     # if the parent of which the current child is generated
@@ -98,14 +95,13 @@ class Graph:
                         # update the list of child's value
                         # add the parent in the child's value list
                         self.connect(child, parent)
-                
+
                 # if the child doesn't exist in the dict
                 else:
                     # create the child key with a empty list in the dict
                     # and append the parent in the list
                     self.connect(child, parent)
 
-                           
     def connect(self, A: str, B: str):
         """
         Create a key if not in the dict OR Update the list of the existing key's value
@@ -114,8 +110,7 @@ class Graph:
         :param B: value to be inserted in the list of the key's value
         """
         self.graph_dict.setdefault(A, []).append(B)
-        
-        
+
     def get(self, a: str):
         """
         Class method to get the value of a key of the dict
@@ -125,7 +120,6 @@ class Graph:
         """
         return self.graph_dict.setdefault(a, {})
 
-    
 
 def UndirectedGraph(graph_dict: dict = None) -> Graph:
     """
@@ -137,12 +131,12 @@ def UndirectedGraph(graph_dict: dict = None) -> Graph:
     return Graph(graph_dict=graph_dict, directed=False)
 
 
-
 class Node:
     """
     Node class
     
     """
+
     def __init__(self, state: str, parent: Node = None):
         """
         constructor of the Node class
@@ -153,13 +147,11 @@ class Node:
         self.state: str = state
         self.parent: Node = parent
 
-    
     def __repr__(self):
         """ Overrides the 'repr' method """
-        
+
         return f"<Node {self.state}>"
 
-    
     def expand(self, problem: GraphProblem) -> list[Node]:
         """
         Expand the current node or state
@@ -169,7 +161,6 @@ class Node:
          """
         return [self.child_node(action) for action in problem.actions(self.state)]
 
-    
     def child_node(self, action: str) -> Node:
         """
         Create child node instance
@@ -177,31 +168,30 @@ class Node:
         :param action: child state
         :returns: child Node instance
          """
-        
+
         # create a child node with the
         # state: child state
         # parent: current Node
         next_node = Node(action, self)
-        
+
         return next_node
 
-    
     def path(self) -> list[Node]:
         """
         Gives the path from start to goal node
 
         """
-        node: Node = self           # node: current Node
-        path_back: list = list()    # path_back: empty list
-              
+        node: Node = self  # node: current Node
+        path_back: list = list()  # path_back: empty list
+
         while node:
             # append the node in the path_back
-            path_back.append(node)  
+            path_back.append(node)
 
             # now the current node is the parent of the current node
-            node = node.parent      
+            node = node.parent
 
-        # returns the reversed list of the path     
+            # returns the reversed list of the path
         return path_back[::-1]
 
 
@@ -221,11 +211,10 @@ def bfs_tree(problem: GraphProblem, verbose: bool = False) -> list | None:
     s: str = ""
     while frontier:
         s += f"queue: {list(frontier)}\n"
-        node: Node = frontier.popleft()     # current node selected from the queue
+        node: Node = frontier.popleft()  # current node selected from the queue
         s += f"Current State: {node.state}\n"
         s += f"After selecting {node} the queue is {list(frontier)}\n"
 
-        
         # checks the node after it is expanded
         if problem.goal_test(node.state):
             if verbose:
@@ -266,12 +255,11 @@ def bfs_graph(problem: GraphProblem, verbose: bool = False) -> list | None:
     s: str = ""
     while frontier:
         s += f"queue: {list(frontier)}\n"
-        node = frontier.popleft()       # select the first element from the queue
+        node = frontier.popleft()  # select the first element from the queue
         s += f"Current State: {node.state}\n"
         s += f"After selecting {node} the queue is {list(frontier)}\n"
-        explored.add(node.state)        # add the node to the visited set
+        explored.add(node.state)  # add the node to the visited set
 
-        
         s += f"Children of {node.state} : {node.expand(problem)}\n"
         for child in node.expand(problem):
             # if the child not in explored and not in the queue 
@@ -283,7 +271,6 @@ def bfs_graph(problem: GraphProblem, verbose: bool = False) -> list | None:
                 frontier.append(child)
         s += f"After inserting, queue: {list(frontier)}\n\n"
 
-
     # if the path is not found then returns None
     return None
 
@@ -291,14 +278,13 @@ def bfs_graph(problem: GraphProblem, verbose: bool = False) -> list | None:
 # print the path
 def print_path(path: list):
     [print(p.state, end=" => ") if i != len(path) - 1 else print(p.state) for i, p in enumerate(path)]
-            
 
 
 if __name__ == "__main__":
     # data for the problem of travelling Romania
-    romania_map_graph: dict['str', list['str',...]] = dict(
-        Arad = ['Zerind', 'Sibiu', 'Timisoara'],
-        Bucharest = ["Urziceni", "Pitesti", "Giurgiu", "Fagaras"],
+    romania_map_graph: dict['str', list['str', ...]] = dict(
+        Arad=['Zerind', 'Sibiu', 'Timisoara'],
+        Bucharest=["Urziceni", "Pitesti", "Giurgiu", "Fagaras"],
         Craiova=["Drobeta", "Rimnicu", "Pitesti"],
         Drobeta=["Mehadia"],
         Eforie=["Hirsova"],
@@ -321,6 +307,6 @@ if __name__ == "__main__":
 
     print("\n\nGraph Search")
     print_path(graph_search_path)
-            
+
     print("\nTree Search")
     print_path(tree_search_path)
